8278378941d4bb423c0e91c29d2e16fceb784bd7
PaulsGitH <20096590@mail.wit.ie>
2023-12-02 19:47:02 +0000

    Made changes to gradle to allow yaml, xml and json persistence to work correctly, added XML to support saving of both issue and comic details

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,19 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("io.github.microutils:kotlin-logging:3.0.5")
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
+    implementation("org.slf4j:slf4j-simple:2.0.9")
+
+//For Streaming to XML and JSON
+    implementation("com.thoughtworks.xstream:xstream:1.4.18")
+    implementation("org.codehaus.jettison:jettison:1.4.1")
+
+//For YAML persistence
+    // https://mvnrepository.com/artifact/com.fasterxml.jackson.dataformat/jackson-dataformat-yaml
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.16.0-rc1")
+
+
 }
 
 tasks.test {
