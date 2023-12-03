527169ff3924ae8f4c9443b36c3b9128ce83a7fc
PaulsGitH <20096590@mail.wit.ie>
2023-12-03 15:28:21 +0000

    Made changes to allow comic fields and issue fields to work together as a comic series, then issue details, with persistence bugs fixed to allow for both to be saved

--- src/main/kotlin/models/Issue.kt
+++ src/main/kotlin/models/Issue.kt
@@ -1,6 +1,7 @@
 package models
 
 data class Issue (var issueId: Int = 0,
+                  var issueNo: Int = 0,
                   var dateOfPublication : String,
                   var rrp: Int = 0,
                   var currentMarketValue: Int = 0,
@@ -8,10 +9,12 @@ data class Issue (var issueId: Int = 0,
                   var condition: String,
                   var isIssueDocumented: Boolean = false)
 {
-
+    //Added so YAML can save and load variables from Issue along with Comic
+    //details and one source for adding a constructor(): this is found in Comic
+    constructor() : this(0,0, "", 0, 0, "", "", false)
     override fun toString(): String {
         val status = if (isIssueDocumented) "Fully Documented" else "Documentation in Progress"
-        return "$issueId: $dateOfPublication, RRP: $rrp, Current Market Value: $currentMarketValue, Rarity: $rarity, Condition: $condition ($status)"
+        return "$issueId: Issue No: $issueNo, $dateOfPublication, RRP: $rrp, Current Market Value: $currentMarketValue, Rarity: $rarity, Condition: $condition ($status)"
     }
 
 }
